1879aca04b87ef77d267231909a9ba34d4a899bb
Zyzous <160857304+Zyzous@users.noreply.github.com>
2024-02-22 07:36:08 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -6,3 +6,17 @@ st.header("En este espacio comienzo a desarroollar mis aplicaciones")
 st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('Sin título-1_Mesa de trabajo 1.jpg')
 st.image(image, caption='Sin título-1_Mesa de trabajo 1')
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
+
+st.subheader("Ahora usemos 2 Columnas")
+
+col1, col2 = st.columns(2)
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia del usuario")
+  resp = st.checkbox('Estoy de acuerdo')
+  if resp:
+    st.write('Correcto!')
+
