3c48de4fe7aaf089da108dde8b89fb488ca797b1
Zyzous <160857304+Zyzous@users.noreply.github.com>
2024-02-22 07:22:42 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,3 +1,7 @@
 import streamlit as st
 from PIL import Image
 st.title("Hola mundo")
+
+st.header("En este espacio comienzo a desarroollar mis aplicaciones")
+st.write("Facilmente puedo realizar backend y frontend.")
+image = Image.open('Interfaces Mult2.png')
